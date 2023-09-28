3ca0e184ed8c6c264b57f9f5ec6451a904687d96
Mateusz Ossoliński <mateusz.ossolinski@gmail.com>
2023-09-28 06:36:16 +0200

    added single entry page content, updated prisma schema

--- app/(dashboard)/journal/[id]/page.tsx
+++ app/(dashboard)/journal/[id]/page.tsx
@@ -1,8 +1,27 @@
+import Editor from "@/components/Editor"
+import { getUserByClerkId } from "@/utils/auth"
+import { prisma } from "@/utils/db"
 
-const JournalEntryPage = ({ params }) => {
+const getEntry = async (id: string) => {
+  const user = await getUserByClerkId()
+  const entry = await prisma.journalEntry.findUnique({
+    where: {
+      userId_id: {
+        userId: user?.id,
+        id,
+      }
+    }
+  })
+
+  return entry
+}
+
+const JournalEntryPage = async ({ params }) => {
+  const entry = await getEntry(params.id)
 
   return (
     <div>
+      <Editor entry={entry} />
       {params.id}
     </div>
   )
---
+++ components/Editor.tsx
@@ -0,0 +1,11 @@
+'use client'
+
+const Editor = ({ entry }) => {
+  return (
+    <div>
+      <h1>{entry.content}</h1>
+    </div>
+  )
+}
+
+export default Editor
