4474c06c5cdfb89409d1d6ca0407b61c84107493
Mateusz Ossoliński <mateusz.ossolinski@gmail.com>
2023-10-05 06:53:33 +0200

    displayed history of journal score on the chart

--- app/(dashboard)/history/page.tsx
+++ app/(dashboard)/history/page.tsx
@@ -1,17 +1,36 @@
+import HistoryChart from "@/components/HistoryChart";
 import { getUserByClerkId } from "@/utils/auth";
 import { prisma } from "@/utils/db";
 
 const getData = async () => {
   const user = await getUserByClerkId()
   const analysis = await prisma.analysis.findMany({
-    where: {}
+    where: {
+      userId: user?.id,
+    },
+    orderBy: {
+      createdAt: 'asc',
+    }
+    // select: {
+    //   sentimentScore: true,
+    // }
   })
+
+  const sum = analysis.reduce((all, current) => all + current.sentimentScore, 0)
+  const avg = Math.round(sum / analysis.length)
+
+  return { analysis, avg }
 }
 
-const History = () => {
+const History = async () => {
+  const { avg, analysis } = await getData()
+
   return (
-    <div>
-      History
+    <div className="w-full h-full">
+      History: {avg}
+      <div className="w-full h-full">
+        <HistoryChart data={analysis} />
+      </div>
     </div>
   )
 }
